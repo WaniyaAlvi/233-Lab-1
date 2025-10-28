aa55c8322c7139efa132e70a11ee04c5ec4cff53
WaniyaAlvi <waniyaalvi05@gmail.com>
2025-10-28 22:56:18 +0500

    Initial commit - uploaded java project files

---
+++ LAB 2/src/lab/pkg2/LAB2.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//package lab.pkg2;
+//import java.util.Scanner;
+//public class LAB2 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("Enter Three Numbers :");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int c = input.nextInt();
+//        if ((a > b) && (a > c)){
+//            System.out.println("First number is greater "+a);
+//        }
+//        else if ((b > a) && (b > c)){
+//            System.out.println("Second number is greater " + b);
+//        }
+//        else if ((c > a) && (c > b)){
+//            System.out.println("Third number is greater " + c);
+//        }
+//    }   
+//}
+
+
+//package lab.pkg2;
+//import java.util.Scanner;
+//public class LAB2 {
+//    
+//    public static void main(String[] args) {
+//        
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("num?");
+//        int a = input.nextInt();
+//        
+//        
+//        if (a > 0){
+//            System.out.println("number is positive");
+//        }
+//        else if (a < 0){
+//            System.out.println("number is negative ");
+//            
+//        }
+//        else if(a == 0){
+//            System.out.println("number is zero");
+//        
+//    }
+//    } 
+//    }
+
+
+//package lab.pkg2;
+//import java.util.Scanner;
+//public class LAB2 {
+//        public static void main(String[] args) {
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("num?");
+//        int a = input.nextInt();
+//        
+//        if ((a >= 25) && (a <= 35)){
+//            System.out.println("REASEARCH ASSISTANT");
+//}
+//        else if ((a >= 36) && (a <= 50)){
+//            System.out.println("JUNIOR LECTURER");
+//}
+//        else if ((a >= 51) && (a <= 65)){
+//            System.out.println("LECTURER");
+//}
+//        else if ((a >= 66) && (a <= 80)){
+//            System.out.println("ASSISTANT PROFESSOR");
+//}
+//        }
+//}
+
+//package lab.pkg2;
+//public class LAB2 {
+//    public static void main(String[] args) {
+//        int limit = 19 ;
+//        int first = 0 ;
+//        int second = 1 ;
+//        int next = 0 ;
+//        System.out.println("Fibonacci Series of " + limit + " generations");
+//        for (int i = 1; i <= limit; i++){
+//            System.out.println(first + " ");
+//            next = first + second;
+//            first = second;
+//            second = next;
+//        }
+//    }
+//}
+        
+
+
+//package lab.pkg2;
+//import java.util.Scanner;
+//public class LAB2 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("a :");
+//        double a =input.nextDouble();
+//        System.out.println("b :");
+//        double b = input.nextDouble();
+//        System.out.println("c :");
+//        double c = input.nextDouble();
+//        
+//        double result = b * b - 4.0 * a * c;
+//        
+//        if (result > 0.0) {
+//            double r1 = (-b + Math.pow(result, 0.5)) / (2.0 * a);
+//            double r2 = (-b + Math.pow(result, 0.5)) / (2.0 * a);
+//            System.out.println("The roots are " + r1 + " and" + r2);
+//        }
+//        else if (result == 0.0){
+//            double r1 = -b / (2.0 * a);
+//            System.out.println("The root is " + r1);
+//        }
+//        else {
+//            System.out.println("The equation has no real roots .");
+//        }
+//        
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+
+
+
